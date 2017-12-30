01ae31aebcc8181de68c945fab1939451b779c8b
Ved Prakash <prakashv@gic.com.sg>
2017-12-30 11:42:13 +0800

    FE Classl

---
+++ FeatureEngineering.py
@@ -0,0 +1,40 @@
+import os
+import sys
+
+# This is the main class for the feature Engineering
+
+
+class FeatureEngineering(object):
+    '''
+    Main class for the feature Engineering class
+    '''
+
+    def __init__(self, dataframe):
+
+        self.dataframe = dataframe # pandas dataframe object
+        # The list to add all the dictionaries. Sklearn CountVectorizer will be used to form the feature matrix
+        self.l = []
+
+        return None
+    
+    def extension(self, name):
+        '''
+        Extract the extraction from the name
+        :param name: filename full path
+        :return: str extension
+        '''
+
+        return None
+
+
+    def main(self):
+        '''
+        Performs the main feature engineering tasks.
+        See the excel file for the feature IDS
+        :return:
+        '''
+
+
+
+        return None
+
